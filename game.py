245c3ac66036cbd3fe9db090f59808d771e6f3ee
mjbiener <michaelbiener@yahoo.com>
2021-02-23 08:59:10 -0500

    attempt w/pipenv issues

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import random
 
 RANKS = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 SUITS = ['♠️', '♣️', '♥️', '♦️']
@@ -8,9 +9,11 @@ class Card:
         self.rank = rank
         self.suit = suit
 
-    def __str__(self):
-        return f'{self.rank} of {self.suit}'
+    # def __str__(self):
+    #     return f'{self.rank} of {self.suit}'
 
+    def show(self):
+        return f'{self.rank} of {self.suit}'
 
 class Deck:
     def __init__(self, ranks, suits):
@@ -23,12 +26,14 @@ class Deck:
     #TODO method to shuffle the deck
     # takes self and returns self with self.cards rearranged randomly
     def shuffle_deck(self):
-        pass
+        random.shuffle(self.cards)
+        return self.cards
+        
 
     # TODO method to deal the top card of the deck to a player
     # takes a player and a deck and adds the top card from the deck to a player's hand
     def deal_card(self):
-        pass
+        return self.cards.pop(0)
 
 
 class Player:
@@ -46,9 +51,11 @@ class Game:
         self.winner = False
 
     # TODO use deal_card() method from Deck class to deal 7 cards to each player
-    def deal_hands():
-        pass
+    def deal_hands(self):
+        for i in range(7):
+            self.deck.deal_card()
 
     # TODO create turn action in which player asks for a card and goes fish according to the rules
     # Determine if the turn was a winning/losing turn
     def turn():
+        pass
